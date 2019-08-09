005e54fcc020e03716a6096a105d7e6f62ea7729
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-08-09 00:06:43 -0600

    idk

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'iger.apps.IgerConfig',
-    'suit',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
