7b6e5f2b10823fa263e34d7eab2cac4d7710720d
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-08-08 23:21:34 -0600

    Update students view
    Cambie el idioma de Django, se cambio la forma en que se muestran los datos del estudiante, se pueden editar, se puede buscar por filtro de semestre y grado a los estudiantes. Se cambio el diseño a uno más fácil de entender.

--- mysite/iger/admin.py
+++ mysite/iger/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Student
 
-admin.site.register(Student)
+#admin.site.register(Student)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('student_carnet', 'student_name', 'student_grade', 'student_semester')
+    list_filter = ('student_grade', 'student_semester')
+
+admin.site.register(Student, AuthorAdmin)
