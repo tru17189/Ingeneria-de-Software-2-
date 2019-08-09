8b98df316f84df60a1eb2ddceb6fe043afd349e6
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-08-09 00:35:13 -0600

    iniciar tarea 2

--- mysite/iger/admin.py
+++ mysite/iger/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-
 from .models import Student
 
 
+
 class AuthorAdmin(admin.ModelAdmin):
+
     list_display = ('student_carnet', 'student_name', 'student_grade', 'student_semester')
     list_filter = ('student_grade', 'student_semester')
 
