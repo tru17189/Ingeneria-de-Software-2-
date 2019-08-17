4412d5333e608f4573c433878c41cc80aff0fd0d
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2019-08-17 16:18:03 -0600

    Update test.py

--- mysite/iger/test.py
+++ mysite/iger/test.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import Student
 from .models import ListaDepartamento
+from .models import Circle
+
 # Create your tests here.
 
 class StudentTestCase(TestCase):
@@ -18,6 +20,17 @@ class ListaDepartamentoTestCase(TestCase):
         pruebaDepartamento = ListaDepartamento.objects.get(pk="Guatemala 2")
         self.assertEqual(pruebaDepartamento.coordinacion, '02')
         self.assertEqual(pruebaDepartamento.departamento, 'Guatemala 2')
+
+class createCircleTestCase(TestCase):
+    def test_get_createCircle:
+        Circle.objects.create(circle_code="010203", name_coor="COBÁN",id_coor="01",num_x="02",circle_number="03")
+        pruebaCirculo = Circle.objects.get(pk="010203")
+        self.assertEqual(pruebaCirculo.circle_code, '010203')
+        self.assertEqual(pruebaCirculo.name_coor, 'COBÁN')
+        self.assertEqual(pruebaCirculo.id_coor, '01')
+        self.assertEqual(pruebaCirculo.num_x, '02')
+        self.assertEqual(pruebaCirculo.circle_number, '03')
+
 
 
 
