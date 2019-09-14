c144b78c0c7dc1051cfdadfdd7d11d9e8d947d64
val17102 <val17102@uvg.edu.gt>
2019-09-13 22:31:56 -0600

    cambios de views

--- iger/urls.py
+++ iger/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('nombre', views.nombre, name='nombre'),
     path('students',views.students,name='students'),
     path('students/31082019/Libro', views.libro, name="Libro"),
-    path('students/<str:student_carnet>/', views.detail, name='detail'),
+    path('students/<str:carnet>/', views.detail, name='detail'),
 	path('students/31082019/libro_mate', views.libro_mate, name='libro_mate')
     ]
