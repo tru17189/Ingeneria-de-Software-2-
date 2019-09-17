781013bb5ea075cd4a1a3624f0cc87496c29dd68
tru17189 <32879914+tru17189@users.noreply.github.com>
2019-09-17 17:35:32 -0600

    ingles

--- iger/urls.py
+++ iger/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('students/31082019/Libro', views.libro, name="Libro"),
     path('students/<str:carnet>/', views.detail, name='detail'),
 	path('students/31082019/libro_mate', views.libro_mate, name='libro_mate'),
-	path('students/31082019/libro_mate1', views.libro_mate1, name='libro_mate1')
+	path('students/31082019/libro_mate1', views.libro_mate1, name='libro_mate1'),
+	path('students/31082019/libro_Ingles1', views.libro_Ingles1, name='libro_Ingles1'),
+	path('students/31082019/libro_Fisica1', views.libro_Fisica1, name='libro_Fisica1')
     ]
