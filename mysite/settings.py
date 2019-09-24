17bd9a0315b4f103241a635bf98ecc03aef2991d
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-09-24 16:32:04 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zdkk2y#utcl1-28*mgnje+4vi=^7lz6w6aw7x!h+edm^1$m(*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "assets"), os.path.join(BASE_DIR, "booksIger"),
 )
