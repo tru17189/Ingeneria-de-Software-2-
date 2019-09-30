d572e5398a93b2ad0adf9ea6fcbb213c186219b5
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-09-29 20:48:53 -0600

    Updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "assets"), os.path.join(BASE_DIR, "booksIger"),
 )
