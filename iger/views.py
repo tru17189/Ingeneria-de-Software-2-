400b30167636272fbf444f0814a0471c9416d95f
val17102 <val17102@uvg.edu.gt>
2019-10-21 19:24:23 -0600

    Documentacion de views

--- iger/views.py
+++ iger/views.py
@@ -4,54 +4,64 @@ from django.http import HttpResponse
 from .models import Student, Circle, ListaDepartamento
 from django.http import Http404
 from django.http import HttpResponse
+#Lista de diferentes vistas de  la aplicacion y los procesos que
+#se realizan antes de cargar la pagina solicitada
 
 def index(request):
     return render(request, 'iger/index.html')
-
+#Vista de instrucciones para los estudiantes para la descarga y lectura
+#de los libros electroncos
 def instructions(request):
     return render(request, 'iger/instructions.html')
-
+#Vista de visitante que no es estudiante
 def visitante(request):
     return render(request, 'iger/visitante.html')
-
+#Vista de ingreso de carnet del estudiante
 def carnet(request):
     return render(request, 'iger/carnet.html')
-
+#Vista de visualizador de libro electronico de ingles
 def libro(request):
     return render_to_response('iger/Libros/English/story_html5.html')
-
+#Vista de visualizador de libro de visita
 def libro_Visita(request):
     return render(request, 'iger/Libros/visualizadores/libroVisita.html')
-	
+#Vista de visualizador de libro electronico de matematica financiera
 def libro_mate(request):
     return render(request, 'booksIger/Matematica_Financiera/story_html5.html')
-	
+#Vista de visualizador de libro electronico de matematica
 def libro_mate1(request):
     return render(request, 'iger/Libros/visualizadores/libroMate.html')
-	
+#Vista de visualizador de libro electronico de ingles
 def libro_Ingles1(request):
     return render(request, 'iger/Libros/visualizadores/libroIngles.html')
-	
+#Vista de visualizador de libro electronico de fisica de quinto bachillerato
 def libro_Fisica1(request):
     return render(request, 'iger/Libros/visualizadores/libroFisica.html')
-	
+#Vista de visualizador de libro electronico de fisica de cuarto bachillerato
 def libro_FisicaHTML(request):
     return render(request, '../../booksIger/Fisica4to/story.html')
-
+#Vista de ingreso de nombre completo en caso de que el usuario no 
+#conozca su carnet
 def nombre(request):
     return render(request, 'iger/nombre.html')
 	
 def pantallaTemporal(request):
     return render(request, 'iger/pantallaTemporal.html')
 
-
+#Vista de error en caso de que surja un error tipo 404, y que 
+#pueda regresar a la pagina de inicio
 def error_404_view(request, exception):
     return render(request, 'iger/404.html', status=404)
-
+#Vista de error en caso de que surja un error tipo 500, y que 
+#pueda regresar a la pagina de inicio
 def error_500_view(request):
     data ={}
     return render(request, 'iger/404.html', data)
-    
+#Vista de libros disponibles que previamente a cargar, verifica que el carnet 
+#ingresado si sea uno existente, o que el nombre ingresado si pertenezca a 
+#uno de los estudiantes en la base de datos.
+#Luego dependiendo del grado y semestre del estudiante, carga la lista de libros 
+#necesaria
 def portal(request):
     carnet = ''
     nombre_completo = ''
@@ -77,7 +87,9 @@ def portal(request):
             return render(request, 'iger/QuintoSemester.html', {'student': student})
         else:
             return render(request, 'iger/QuintoSemester2.html', {'student': student})
-
+#Proceso por el cual obtenemos la informacion para generar las estadisticas de 
+#la cantidad de veces que un estudiante ha ingresado a una de las paginas de 
+#la aplicacion
 def detail(request):
     carnet = ''
     nombre_completo = ''
